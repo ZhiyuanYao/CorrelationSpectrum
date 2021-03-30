df4df323e874a7c971290914bbdcd5a84df03b65
Zhiyuan Yao <yao.umass@gmail.com>
2021-03-30 16:42:26 +0800

    first commit

---
+++ Procfile
@@ -0,0 +1,1 @@
+web: julia --project dash_input.jl $PORT
---
+++ dash_input.jl
@@ -0,0 +1,70 @@
+#==============================================================================#
+# Description
+# -----------
+#
+#
+#   Version:  1.0
+#   Created:
+#    Author:  Zhiyuan Yao, zhiyuan.yao@icloud.com
+# Institute:  Insitute for Advanced Study, Tsinghua University
+#==============================================================================#
+# Change Log:
+#   *
+#==============================================================================#
+# todo:
+#   *
+#==============================================================================#
+using Dash, DashHtmlComponents, DashCoreComponents
+using PlotlyJS, DelimitedFiles
+using Plots, HTTP
+
+#------------------------------------------------------------------------------
+#  read in data
+#------------------------------------------------------------------------------
+filename = "./corr_spectrum_L14.dat"
+data = readdlm(filename)
+
+app = dash()
+
+app.layout = html_div() do
+    # default value set to be 2
+    dcc_input(id = "input-1", value = "1", type = "text"),
+    html_div() do
+        dcc_graph(id = "power")
+    end
+end
+
+callback!(app, Output("power", "figure"), Input("input-1", "value")) do value
+    n = parse(Int, value)
+    if value == "" || value == nothing
+       n = 1
+       return Plot([1, 2, 3], [1, 2, 3])
+    end
+    ydat = data[n, :]
+    xdat = [i for i in 1:length(ydat)]
+    # alternatively, we can use Plot(xdat, ydat   on the first line
+    return Plot(xdat, ydat,
+                Layout(
+                       xaxis_type = "linear",  # log
+                       xaxis_title = "E_n",
+                       yaxis_title = "λ_n",
+                       # not updated though
+                       # title = "correlation spectrum for $n-th eigenstate",
+                       legend_x = 0,
+                       legend_y = 1,
+                       hovermode = "closest",
+                       transition_duration = 500
+                      ),
+                mode = "markers+lines",
+                marker_size = 8,
+                line_color = "green",
+                marker_color = palette(:tab10)[1]
+               )
+end
+
+# run_server(app, "0.0.0.0", debug=true)
+handler = make_handler(app)
+println("started at localhost:8080")
+# HTTP.serve(handler, HTTP.Sockets.localhost, 8080)
+# Use this for heroku to host
+HTTP.serve(handler, "0.0.0.0", parse(Int,ARGS[1]))
